bb8c8b4d1b8584df29f5d4232d1e9e710aa80329
acrick <49769202+acrick@users.noreply.github.com>
2026-08-18 15:57:14 -0700

    Added a line to OlyCoffe about being at Chucks today

--- olyCoffee.py
+++ olyCoffee.py
@@ -2,3 +2,5 @@ print("I love a good cup of coffee!")
 
 favCoffeeType = input("What is your favorite type of coffee? ")
 print(f"You love {favCoffeeType}!")
+
+print("Today I'm at studying at Chuck's, though the bandwith is signifigatly slower than the library.")
