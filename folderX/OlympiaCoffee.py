4753ef9bee9d83a9ebf601b25b2ac6ad68efa276
Alex Crick <crickontour@gmail.com>
2026-08-17 17:09:44 -0700

    added two more lines to OlympiaCoffee.py

--- folderX/OlympiaCoffee.py
+++ folderX/OlympiaCoffee.py
@@ -1,2 +1,4 @@
 print("who doesn't love a good cup of coffee!")
 print("Olympia Coffee is the best place to get your caffeine fix.")
+print("Whether you like espresso, latte, or cappuccino, we've got you covered.")
+print("Come visit us and enjoy a delicious cup of coffee today!")
