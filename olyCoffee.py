7773bc1d888d3ad4ee4dd2013dd108edbc11c4d0
Alex Crick <crickontour@gmail.com>
2026-08-18 15:59:35 -0700

    Added a merge conflit message to olyCoffee.py

--- olyCoffee.py
+++ olyCoffee.py
@@ -2,3 +2,5 @@ print("I love a good cup of coffee!")
 
 favCoffeeType = input("What is your favorite type of coffee? ")
 print(f"You love {favCoffeeType}!")
+
+print("This is a change to start a merge conflict")
